ef57138f976a95178babd9a4e28b1ba9c67af93c
Rodrigo <rocoliveira@hotmail.com>
2026-03-26 19:58:03 -0300

    Vincula os dados de um objeto del ista a uma linha especifica da tela

--- app/src/main/java/com/joaovinicius/cadastrodeusuario/UserAdapter.java
+++ app/src/main/java/com/joaovinicius/cadastrodeusuario/UserAdapter.java
@@ -34,6 +34,18 @@ public class UserAdapter{
            //Aqui usamos um layoutpadrão do Android (simple_list_item_1) para facilitar
             View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
 
+            //Retorna uma instância da nossa classe interna ViewHolder com a View criada
+            return new ViewHolder(View);
+        }
+
+        //Metodo2: Vincula os dados de um objeto del ista a uma linha especifica da tela
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position){
+            // Recupera os dados da lista de acordo com a posição que o Android está desenhando agora
+            String nome = listaUsuarios.get(position);
+
+            //define o texto no componete visual que está guardado dentro de 'holder'
+            holder.tvNome.setText(nome);
         }
     }
 }
